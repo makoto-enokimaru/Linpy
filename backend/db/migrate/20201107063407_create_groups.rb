c77950bd6a1aee59ca5a8fd2c169194c08162540
makoto-enokimaru <m.k.t.intruder@gmail.com>
2020-11-07 16:54:25 +0900

    GroupテーブルとUserテーブルを作成

--- backend/db/migrate/20201107063407_create_groups.rb
+++ backend/db/migrate/20201107063407_create_groups.rb
@@ -1,13 +1,13 @@
 class CreateGroups < ActiveRecord::Migration[6.0]
   def change
     create_table :groups do |t|
-      t.string :groupid
-      t.string :groupname
-      t.string :groupcode
-      t.text :groupprofile
+      t.string :group_id
+      t.string :group_name
+      t.string :group_code
+      t.text :group_profile
       t.string :rep_postcode
       t.text :rep_address
-      t.string :rep_tel
+      t.string :rep_phone_number
       t.string :rep_email
       t.string :status
 
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,6 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_11_07_070449) do
 
+  create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "group_id"
+    t.string "group_name"
+    t.string "group_code"
+    t.text "group_profile"
+    t.string "rep_postcode"
+    t.text "rep_address"
+    t.string "rep_phone_number"
+    t.string "rep_email"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.string "user_id"
+    t.string "user_name"
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "email"
+    t.string "password"
+    t.string "biography"
+    t.text "role_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["group_id"], name: "index_users_on_group_id"
+  end
+
+  add_foreign_key "users", "groups"
 end
