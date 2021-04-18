95da05cff6c6e7aab44a9f7c5e927601b5025047
makoto-enokimaru <m.k.t.intruder@gmail.com>
2021-04-18 16:33:00 +0900

    test

---
+++ frontend/nuxt.config.js
@@ -0,0 +1,46 @@
+export default {
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  server: {
+    port: 3000, 
+    host: '172.20.0.4' // docker-compose.ymlで割り当てたもの
+  },
+
+  head: {
+    title: 'Linpy',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {},
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+  }
+}
